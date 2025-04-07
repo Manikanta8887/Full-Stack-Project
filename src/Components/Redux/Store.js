2fb8969bc342e3b05d2576897e9f253e3187ced9
Manikanta8887 <swamimanikanta66@gmail.com>
2025-04-07 13:12:26 +0530

    Frontend

--- src/Components/Redux/Store.js
+++ src/Components/Redux/Store.js
@@ -1,15 +1,3 @@
-// import { configureStore } from "@reduxjs/toolkit";
-// import userReducer, { setFirebaseUser, logoutUser, updateUser, updateProfilePic } from "./userSlice.js";
-
-// const store = configureStore({
-//   reducer: {
-//     user: userReducer,
-//   },
-// });
-
-// export { setFirebaseUser, logoutUser, updateUser, updateProfilePic };
-// export default store;
-
 // src/redux/store.js
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./userSlice";
--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,3 @@
-// import { StrictMode } from 'react'
-// import { createRoot } from 'react-dom/client'
-// import { BrowserRouter } from 'react-router-dom'
-// import App from './App.jsx'
-// import store from './Components/Redux/Store.js'
-// import { Provider } from 'react-redux'
-// import './index.css'
-
-// createRoot(document.getElementById('root')).render(
-//   <StrictMode>
-//     <BrowserRouter>
-//     <Provider store={store}>
-//       <App />
-//       </Provider>
-//     </BrowserRouter>
-//   </StrictMode>
-// )
-
-
 // index.jsx
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
